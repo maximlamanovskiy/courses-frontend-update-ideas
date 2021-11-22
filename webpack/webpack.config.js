8b5e0465837cf10c86b7ac7b856c7ed5277f17aa
Maxim Lamanovsky <maxim.lamanovsky@7bits.it>
2021-11-22 11:55:00 +0600

    Done with 4 workshop with webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,21 +2,21 @@ const path = require("path");
 const HandlebarsPlugin = require("handlebars-webpack-plugin");
 const WebpackConcatPlugin = require('webpack-concat-files-plugin');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const { web } = require("webpack");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
     output: {
         path: path.resolve(__dirname, "build"),
     },
 
-    target: "web",
+    mode: "development",
 
     plugins: [
         new HandlebarsPlugin({
             entry: path.resolve(__dirname, "src", "pages", "*", "*.hbs"),
             output: path.resolve(__dirname, "build", "[name].html"),
             partials: [
-                path.resolve(__dirname, "src", "components", "*", "*.hbs")
+                path.resolve(__dirname, "src", "components", "*", "*.hbs"),
             ],
         }),
         new WebpackConcatPlugin({
@@ -24,20 +24,26 @@ module.exports = {
                 {
                     dest: path.resolve(__dirname, "build", "styles", "main.css"),
                     src: path.resolve(__dirname, "src", "**", "*.css"),
-                }
-            ]
+                },
+            ],
         }),
         new CopyWebpackPlugin({
             patterns: [
                 {
                     from: path.resolve(__dirname, "src", "**", "images", "*.png"),
                     to: path.resolve(__dirname, "build", "images", "[name].png"),
-                }
-            ]
+                },
+            ],
         }),
+        new BrowserSyncPlugin(
+            {
+                host: "localhost",
+                port: 3000,
+                server: {baseDir: path.resolve(__dirname, "build")},
+            },
+            {
+                reload: true,
+            },
+        ),
     ],
-
-    devServer: {
-        open: true,
-    },
 }
